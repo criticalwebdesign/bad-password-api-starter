26445f6026ae96b381dd4c582683a6cffc3e2c71
Owen Mundy <omundy@gmail.com>
2026-01-07 16:43:07 -0500

    Format / sync

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,20 @@ let button = document.querySelector("#submit");
 // EVENTS
 window.addEventListener("load", updatePassword);
 document.addEventListener("submit", (e) => {
-  e.preventDefault();
-  updatePassword();
+    e.preventDefault();
+    updatePassword();
 });
 
 // add a change event listener to each option
 document.querySelectorAll("input[type=radio]").forEach((ele) => {
-  ele.addEventListener("change", updatePassword);
+    ele.addEventListener("change", updatePassword);
 });
 
 // called from load and user events
 async function updatePassword() {
-  // 👉 add code inside this function (from Chapter 9) ...
+    // 👉 add code inside this function (from Chapter 9) ...
 
 
+
+    // 👈
 }
